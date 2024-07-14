4036ad1f11a4cfb8ffb866d437bba45a161ef39d
Laz627 <169095963+Laz627@users.noreply.github.com>
2024-07-14 16:50:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def get_cluster_name(cluster_keywords, min_words=1, max_words=3):
     word_counts = Counter(words)
     
     # Define words to exclude
-    exclude_words = set(['for', 'what', 'why', 'how', 'when', 'where', 'it', 'which', 'who', 'whom', 'whose', 'a', 'an', 'the', 'and', 'or', 'but', 'in', 'on', 'at', 'to', 'of', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'do', 'does', 'did', 'will', 'would', 'shall', 'should', 'may', 'might', 'must', 'can', 'could'])
+    exclude_words = set(['for', 'what', 'why', 'how', 'when', 'where', 'it', 'which', 'who', 'whom', 'whose', 'a', 'an', 'the', 'and', 'or', 'but', 'in', 'on', 'at', 'to', 'of', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'do', 'does', 'did', 'will', 'would', 'shall', 'should', 'may', 'might', 'must', 'can', 'could', 'that'])
     
     # Get the most common words, excluding certain words
     common_words = [word for word, count in word_counts.most_common() 
